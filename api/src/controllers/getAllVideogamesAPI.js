3f85c4713b1195a82fde5f77853846c586b36488
Matias-abh <matias.abh@gmail.com>
2023-03-19 20:44:41 -0300

    feat(GetAllVg): bring 10 pages

--- api/src/controllers/getAllVideogamesAPI.js
+++ api/src/controllers/getAllVideogamesAPI.js
@@ -5,7 +5,7 @@ const getAllVideogamesAPI = async () => {
     let allVideogamesAPI = [];
 
     try {
-        for (let i = 1; i <= 5; i++) {
+        for (let i = 1; i <= 10; i++) {
             const dataAPI = (await axios(`https://api.rawg.io/api/games?key=${API_KEY}&page=${i}`)).data.results;
                                           
             allVideogamesAPI = [ ...allVideogamesAPI, ...dataAPI];
